b00ed0b121cd00e759a5dff352946b2d15570fe6
Cossack Proger <yiksnelots@gmail.com>
2019-05-30 15:26:02 +0300

    config settings

--- Avia_API/settings.py
+++ Avia_API/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 DEBUG = True
 # DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['avia_api.herokuapp.com']
 
 # Application definition
 
